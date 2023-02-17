b54382b04c908a21c0f5680806bdfcd9912d6f5d
Nicholas Maglowsch Salgueiro <nickmaglowsch3@gmail.com>
2023-02-17 15:27:16 -0300

    unit tests and husky

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/*.test.ts'],
+    collectCoverage: true,
+    coverageReporters: ['lcov', 'text', 'html'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/**/*.test.{js,jsx,ts,tsx}',
+        '!src/index.{js,jsx,ts,tsx}',
+    ],
 };
